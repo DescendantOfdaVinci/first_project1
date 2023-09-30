935d944316f22402b766edc349e2b42a3b26ecae
DescendantOfdaVinci <128248529+DescendantOfdaVinci@users.noreply.github.com>
2023-09-30 19:15:12 +0500

    Add files via upload

---
+++ job/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', junior),
+    path('middle/', middle),
+    path('senior/', senior)
+]
